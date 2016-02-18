91a14f4f36989f193c1973ae395f00626227ee66
CodingLi <passbyshell@foxmail.com>
2016-02-18 22:44:41 +0800

    add new database and relative class

---
+++ app/src/main/java/model/Country.java
@@ -0,0 +1,44 @@
+package model;
+
+/**
+ * Created by Keen on 02/18/2016.
+ */
+public class Country {
+
+    private int id;
+    private String countryName;
+    private String countryCode;
+    private int cityId;
+
+    public int getId() {
+        return id;
+    }
+
+    public String getCountryName() {
+        return countryName;
+    }
+
+    public String getCountryCode() {
+        return countryCode;
+    }
+
+    public int getCityId() {
+        return cityId;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setCountryName(String countryName) {
+        this.countryName = countryName;
+    }
+
+    public void setCountryCode(String countryCode) {
+        this.countryCode = countryCode;
+    }
+
+    public void setCityId(int cityId) {
+        this.cityId = cityId;
+    }
+}
